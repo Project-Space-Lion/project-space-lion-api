1643e0f88c7c66ae3b342706e82657b15d993b25
perrone13 <perrone.13@osu.edu>
2021-04-08 16:47:47 -0400

    Part 2

---
+++ backend/Space.Lion.Api/Controllers/CatalogController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Space.Lion.Domain.Catalog;
+using System.Collections.Generic;
+
+
+namespace Space.Lion.Api.Controllers 
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            var items = new List<Item>()
+            {
+                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+
+            };
+
+            return Ok(items);
+        }
+    }
+}
