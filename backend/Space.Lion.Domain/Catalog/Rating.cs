0667310165295c5fd2f1d30b51f9021e9b5d15f2
Sydney Mathias <sydneymathias@gmail.com>
2021-04-08 16:03:51 -0400

    Catalog Commit

---
+++ backend/Space.Lion.Domain/Catalog/Rating.cs
@@ -0,0 +1,30 @@
+
+using System;
+
+namespace Space.Lion.Domain.Catalog
+{
+    public class Rating
+    {
+        public Rating(int stars, string userName, string review)
+        {
+            if(stars < 1 || stars > 5)
+            {
+                throw new ArgumentException("Star rating must be an integer: 1, 2, 3, 4, OperatingSystem 5.");
+            }
+            if(string.IsNullOrEmpty(userName))
+            {
+                throw new ArgumentException("UserName cannot be null.");
+            }
+
+            this.Star = stars;
+            this.UserName = userName;
+            this.Review = review;
+        }
+    
+
+        public int Star { get; set; }
+
+        public string UserName { get; set; }
+        public string Review { get; set; }
+    }
+}
