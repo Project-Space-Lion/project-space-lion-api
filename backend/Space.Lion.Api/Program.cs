681003cafe41bc23c840dfbc103fc00a65017916
sbuule48 <sbuule48@gmail.com>
2021-04-16 01:43:53 -0400

    Part 4

--- backend/Space.Lion.Api/Program.cs
+++ backend/Space.Lion.Api/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Space.Lion.Data;
 
 namespace Space.Lion.Api
 {
@@ -14,6 +16,9 @@ namespace Space.Lion.Api
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            	var host = CreateHostBuilder(args).Build();
+                CreateDbIfNotExists(host);
+                host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +27,24 @@ namespace Space.Lion.Api
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scopre = host.Services.CreateScope())
+            {
+                var services = Scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context, logger);
+                }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "Error occured creating the database.");
+                    }
+            }
+        }
     }
 }
